f37769c1f18bbb76641a937bda3dfac8a5b76e6b
Geoffrey Bachelet <grosfrais@gmail.com>
2014-01-22 08:34:39 +0100

    fixed docblock

--- src/Docker/Context/ContextInterface.php
+++ src/Docker/Context/ContextInterface.php
@@ -2,12 +2,15 @@
 
 namespace Docker\Context;
 
+/**
+ * Docker\Context\ContextInterface
+ */
 interface ContextInterface
 {
     /**
      * Get context as a stream
      *
-     * return ressource Return a stream ressource
+     * @return ressource Return a stream ressource
      */
     public function toStream();
 }

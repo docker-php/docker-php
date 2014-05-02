75ac530354226d2295f57dcde4699fa1625ea018
Geoffrey Bachelet <grosfrais@gmail.com>
2014-05-02 09:11:19 +0200

    Fixed travis run
    There's a bug in Travis' PHP support where /etc/profile.d/phpenv.sh
    is not sourced accordingly, thus making phpenv (and shims) unavaible
    in the PATH

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export PATH=$PATH:/home/travis/.phpenv/bin
+eval "$(phpenv init -)"
+phpenv rehash 2>/dev/null
+
 # Exit on first error
 set -e
 
@@ -11,4 +15,4 @@ docker -d -H 0.0.0.0:4243 -H unix:///var/run/docker.sock 2>> /dev/null >> /dev/n
 sleep 2
 
 # $init is set by sekexe
-cd $(dirname $init)/.. && /home/travis/.phpenv/shims/php bin/phpunit -c phpunit.xml.dist
+cd $(dirname $init)/.. && php bin/phpunit -c phpunit.xml.dist

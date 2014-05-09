a9f5e24ebe6c6156d2a2dee5de28a88d721c0058
Joel Wurtz <jwurtz@jolicode.com>
2014-05-09 18:13:14 +0200

    Fix travis build

--- docker.sh
+++ docker.sh
@@ -10,6 +10,10 @@ set -e
 # Kill background processes on exit
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 
+# Workaround with cgroup for recent version of docker
+cgroups-umount
+cgroups-mount
+
 # Start docker daemon
 docker -d -H 0.0.0.0:4243 -H unix:///var/run/docker.sock 2>> /dev/null >> /dev/null &
 sleep 2

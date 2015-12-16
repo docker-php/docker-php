e2fc604d2cb365dd375b502c7e67543290ee483b
Matěj Koudelka <matej.koudelka@netrex.cz>
2015-12-16 15:30:15 +0100

    fix issue https://github.com/stage1/docker-php/issues/145

--- src/Docker/Http/DockerClient.php
+++ src/Docker/Http/DockerClient.php
@@ -55,7 +55,7 @@ class DockerClient extends Client
     }
 
     /**
-     * Create a docker client from environnement variable
+     * Create a docker client from environment variable
      *
      * @param array $config Config for the http client (guzzle)
      *
@@ -71,7 +71,7 @@ class DockerClient extends Client
 
         if (getenv('DOCKER_TLS_VERIFY') && getenv('DOCKER_TLS_VERIFY') == 1) {
             if (!getenv('DOCKER_CERT_PATH')) {
-                throw new \RuntimeException('Connection to docker has been set to use TLS, but no PATH is defined for certificate in DOCKER_CERT_PATH docker environnement variable');
+                throw new \RuntimeException('Connection to docker has been set to use TLS, but no PATH is defined for certificate in DOCKER_CERT_PATH docker environment variable');
             }
 
             $useTls = true;
@@ -79,15 +79,12 @@ class DockerClient extends Client
             $certfile = getenv('DOCKER_CERT_PATH').DIRECTORY_SEPARATOR.'cert.pem';
             $keyfile = getenv('DOCKER_CERT_PATH').DIRECTORY_SEPARATOR.'key.pem';
             $peername = getenv('DOCKER_PEER_NAME') ? getenv('DOCKER_PEER_NAME') : 'boot2docker';
-            $fullcert = tempnam(sys_get_temp_dir(), 'docker-certfile');
-
-            file_put_contents($fullcert, file_get_contents($certfile));
-            file_put_contents($fullcert, file_get_contents($keyfile), FILE_APPEND);
 
             $context = stream_context_create([
                 'ssl' => [
                     'cafile' => $cafile,
-                    'local_cert' => $fullcert,
+                    'local_cert' => $certfile,
+                    'local_pk' => $keyfile,
                     'peer_name' => $peername,
                 ],
             ]);

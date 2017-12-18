18a9b73c474406c04c61f253ee134c771de8c9f0
Joel Wurtz <jwurtz@jolicode.com>
2017-12-18 17:32:47 +0100

    Workaround for async urls, add a small test as an example

---
+++ tests/DockerAsyncTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Docker\Tests;
+
+use Amp\Loop;
+use Docker\API\Model\ContainersCreatePostBody;
+use Docker\DockerAsync;
+
+class DockerAsyncTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAsync()
+    {
+        Loop::run(function () {
+            $docker = DockerAsync::create();
+
+            $containerConfig = new ContainersCreatePostBody();
+            $containerConfig->setImage('busybox:latest');
+            $containerConfig->setCmd(['echo', '-n', 'output']);
+            $containerConfig->setAttachStdout(true);
+            $containerConfig->setLabels(new \ArrayObject(['docker-php-test' => 'true']));
+
+            $containerCreate = yield $docker->containerCreate($containerConfig);
+            $containerStart = yield $docker->containerStart($containerCreate->getId());
+            $containerInfo = yield $docker->containerInspect($containerCreate->getId());
+
+            $this->assertEquals($containerCreate->getId(), $containerInfo->getId());
+        });
+    }
+}

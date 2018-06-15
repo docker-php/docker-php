770a1fb939e8a1aaf9ceca100df77f111a6961d2
José Fernando Cordova <josefernando.c@geopagos.com>
2018-06-15 14:10:52 -0300

    ServiceNormalizer Fixed

---
+++ generated/Normalizer/ServiceNormalizer.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Docker\API\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\SerializerAwareNormalizer;
+
+class ServiceNormalizer extends SerializerAwareNormalizer implements DenormalizerInterface, NormalizerInterface
+{
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        if ($type !== 'Docker\\API\\Model\\Service') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        if ($data instanceof \Docker\API\Model\Service) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $dataCreatedAtString = $data->{'CreatedAt'};
+        $dataCreatedAtString = explode(".", $dataCreatedAtString)[0]."UTC";
+        
+        $dataUpdatedAtString = $data->{'UpdatedAt'};
+        $dataUpdatedAtString = explode(".", $dataUpdatedAtString)[0]."UTC";
+
+        $object = new \Docker\API\Model\Service();
+        if (property_exists($data, 'ID')) {
+            $object->setID($data->{'ID'});
+        }
+        if (property_exists($data, 'Version')) {
+            $object->setVersion($this->serializer->deserialize($data->{'Version'}, 'Docker\\API\\Model\\NodeVersion', 'raw', $context));
+        }
+        if (property_exists($data, 'CreatedAt')) {
+            $object->setCreatedAt(\DateTime::createFromFormat("Y-m-d\TH:i:sP", $dataCreatedAtString));
+        }
+        if (property_exists($data, 'UpdatedAt')) {
+            $object->setUpdatedAt(\DateTime::createFromFormat("Y-m-d\TH:i:sP", $dataUpdatedAtString));
+        }
+        if (property_exists($data, 'Spec')) {
+            $object->setSpec($this->serializer->deserialize($data->{'Spec'}, 'Docker\\API\\Model\\ServiceSpec', 'raw', $context));
+        }
+        if (property_exists($data, 'Endpoint')) {
+            $object->setEndpoint($this->serializer->deserialize($data->{'Endpoint'}, 'Docker\\API\\Model\\Endpoint', 'raw', $context));
+        }
+        if (property_exists($data, 'UpdateStatus')) {
+            $object->setUpdateStatus($this->serializer->deserialize($data->{'UpdateStatus'}, 'Docker\\API\\Model\\UpdateStatus', 'raw', $context));
+        }
+
+        return $object;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = new \stdClass();
+        if (null !== $object->getID()) {
+            $data->{'ID'} = $object->getID();
+        }
+        if (null !== $object->getVersion()) {
+            $data->{'Version'} = $this->serializer->serialize($object->getVersion(), 'raw', $context);
+        }
+        if (null !== $object->getCreatedAt()) {
+            $data->{'CreatedAt'} = $object->getCreatedAt()->format("Y-m-d\TH:i:sP");
+        }
+        if (null !== $object->getUpdatedAt()) {
+            $data->{'UpdatedAt'} = $object->getUpdatedAt()->format("Y-m-d\TH:i:sP");
+        }
+        if (null !== $object->getSpec()) {
+            $data->{'Spec'} = $this->serializer->serialize($object->getSpec(), 'raw', $context);
+        }
+        if (null !== $object->getEndpoint()) {
+            $data->{'Endpoint'} = $this->serializer->serialize($object->getEndpoint(), 'raw', $context);
+        }
+        if (null !== $object->getUpdateStatus()) {
+            $data->{'UpdateStatus'} = $this->serializer->serialize($object->getUpdateStatus(), 'raw', $context);
+        }
+
+        return $data;
+    }
+}
